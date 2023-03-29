094ef9392a9cf1a3b931267bdd00b45fba22f966
egoing <egoing@gmail.com>
2023-03-29 11:47:00 +0900

    CSS module

--- 0329/src/App.js
+++ 0329/src/App.js
@@ -1,5 +1,7 @@
 import {useState} from 'react';
 import './App.css';
+import counterStyle from './Counter.module.css';
+console.log('counterStyle', counterStyle);
 function Counter({title, initValue}){
   const [count, setCount] = useState(initValue);
   console.log('Counter', count);
@@ -17,14 +19,18 @@ function Counter({title, initValue}){
   return (
     <div style={design}>
       <h1 className="title">{title}</h1>
-      <button onClick={up}>+</button> {count}
+      <button onClick={up} className={counterStyle.rightSpace}>+</button> {count}
     </div>
   )
+}
+function Counter2(){
+  return <h1 className="title">Counter2</h1>
 }
 function App() {
   return (
     <div className="App">
       <Counter title="카운터" initValue={10}></Counter>
+      <Counter2></Counter2>
     </div>
   );
 }
