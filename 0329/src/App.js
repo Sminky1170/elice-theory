45f667bccffa3d4b38da6d9ece99ab864c9db0f6
egoing <egoing@gmail.com>
2023-03-29 10:54:49 +0900

    callback style state

---
+++ 0329/src/App.js
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+function Counter({title, initValue}){
+  const [count, setCount] = useState(initValue);
+  console.log('Counter', count);
+  function up(){
+    setCount(count+1);
+    // setCount(function(oldCount){
+    //   console.log('oldCount', oldCount);
+    //   return oldCount+1;
+    // })    
+  }
+  return (
+    <div>
+      <h1>{title}</h1>
+      <button onClick={up}>+</button> {count}
+    </div>
+  )
+}
+function App() {
+  return (
+    <div className="App">
+      <Counter title="카운터" initValue={10}></Counter>
+    </div>
+  );
+}
+
+export default App;
