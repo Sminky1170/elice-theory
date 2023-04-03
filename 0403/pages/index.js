7f21292d82de743e5b7a2b9e3c2466ff6884eafb
egoing <egoing@gmail.com>
2023-04-03 13:06:53 +0900

    기본 UI 제작

--- 0403/pages/index.js
+++ 0403/pages/index.js
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
     <>
-      hello world
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/read/1">html</a></li>
+        <li><a href="/read/2">css</a></li>
+        <li><a href="/read/3">js</a></li>
+      </ol>
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
+      <ul>
+        <li><a href="/create">Create</a></li>
+        <li><a href="/update">Update</a></li>
+        <li><a href="/delete">Delete</a></li>
+      </ul>
     </>
   )
 }
