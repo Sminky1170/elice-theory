602d7e82e9ffeadd81c87e9e35ccde257b93820f
egoing <egoing@gmail.com>
2023-04-05 11:34:46 +0900

    create 구현

--- 0403/pages/create.js
+++ 0403/pages/create.js
@@ -1,9 +1,35 @@
-import Layout from '../components/layout';
+import { useRouter } from "next/router";
+
 export default function Create(){
+    const router = useRouter();
+    const handler = (evt) => {
+        evt.preventDefault();
+        const title = evt.target.title.value;
+        const body = evt.target.body.value;
+        const data = {
+          title: title,
+          body: body,
+        };
+        fetch('http://localhost:9999/topics', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+          .then((response) => response.json())
+          .then((result) => {
+            console.log('Success:', result);
+            const last_id = result.id;
+            router.push('/read/'+last_id);
+          });
+      };
     return ( 
-        <>
+        <form onSubmit={handler}>
             <h2>Create</h2>
-            Hello, Create
-        </>
+            <p><input type="text" name="title" placeholder='제목' /></p>
+            <p><textarea name="body" placeholder="본문"></textarea></p>
+            <p><input type="submit" value="Create" /></p>
+        </form>
     )
 }
