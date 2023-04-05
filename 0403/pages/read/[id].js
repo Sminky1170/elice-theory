01272a962faf66f05e7e2c031b1a6c679a31bd1e
egoing <egoing@gmail.com>
2023-04-05 10:47:03 +0900

    상세보기 구현

--- 0403/pages/read/[id].js
+++ 0403/pages/read/[id].js
@@ -1,9 +1,20 @@
 import { useRouter } from 'next/router'
 import Layout from '../../components/layout'
+import { useEffect, useState } from 'react';
 export default function Read(){
     const router = useRouter();
+    const [topic, setTopic] = useState({title:'', body:''});
+    useEffect(()=>{
+        if(router.query.id !== undefined) {
+            fetch('http://localhost:9999/topics/' + router.query.id)
+                .then((res) => res.json())
+                .then((data) => {
+                    setTopic(data);
+                })
+        }
+    },[router.query.id]);
     return <>
-        <h2>Read</h2>
-        Hello, Read {router.query.id}
+        <h2>{topic.title}</h2>
+        {topic.body}
     </>
 }
